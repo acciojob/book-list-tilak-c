2432e30bd995745eee3752de6b19a7637e258982
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 12:21:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const submitElement=document.getElementById("submit");
+const tableBody=document.querySelector("#book-list tbody");
+submitElement.addEventListener("click",function(){
+	const title=document.getElementById("title").value.trim();
+	const author=document.getElementById("author").value.trim();
+	const isbn=document.getElementById("isbn").value.trim();
+
+	if(!title||!author||!isbn){
+		alert("Please fill all fields");
+		return;
+	}
+	const row=document.createElement("tr");
+	row.innerHTML=`
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td><button class="delete-btn">X</button></td>
+	`
+		tableBody.appendChild(row);
+	document.getElementById("title").value="";
+	document.getElementById("author").value="";
+	document.getElementById("isbn").value="";
+	row.querySelector(".delete-btn").addEventListener("click",
+				function(){
+		row.remove();
+	})
+	
+})
